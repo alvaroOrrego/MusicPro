2232582602c1a67cec97fbf8af06b9047d66955f
aorre6o <alv.orrego@duocuc.cl>
2022-05-15 22:15:42 -0400

    prueba commit2

--- core/templates/core/index.php
+++ core/templates/core/index.php
@@ -1,2 +1,1 @@
-<?php require once("apiUsd.html", "blank.html", "carrito.html", "checkout.html", 
-                    "get.html", "index.html", "product.html", "store.html", "tienda.html") ?>
+<?php require_once("index.html") ?>
